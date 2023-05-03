dc8b10d4fb903154c4268dfa8a5d1d644dbc34dc
betir <betirov95@gmail.com>
2023-05-03 23:28:19 +0300

    12asd

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pizza-vue",
+  base: "/pizza-vue/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
